2c04c4a2b43489daedc8a6cabd2471e3b2718bef
Adrien <adriengaillard591@gmail.com>
2026-05-20 10:48:58 +0200

    Add map file and sprite sheet

---
+++ Map/tilesheet_finale.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tilesheet_finale" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../Tuiles/tilesheet_finale.png" width="96" height="96"/>
+</tileset>
